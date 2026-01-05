cc654567605a7054f391b50a99e951a4f8771c86
DEAD <hellowassim86@gmail.com>
2026-01-05 10:53:56 +0100

    Update DeadLoader to use new file URL

--- DeadLoader.lua
+++ DeadLoader.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/ac36a7b6e975f127"))()
+--loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/ac36a7b6e975f127"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/59d9ad01782b3660"))()
